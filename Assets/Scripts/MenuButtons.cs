dc0651bce032f0bcf96a3ecf787c96d5eb353684
YonHoshihara <hoshiharyon@gmail.com>
2016-09-14 13:18:32 -0400

    Menu quase ok

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,14 @@
+﻿           using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Fase1");
+    }
+
+}
