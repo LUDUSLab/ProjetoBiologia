5310d0bb54c020a41e403e7c0337a0ca7a3e042c
Aline Lima <lima.alinee@gmail.com>
2016-09-16 10:47:07 -0400

    Changes

--- Assets/Scripts/Personagem/indiozinho.cs
+++ Assets/Scripts/Personagem/indiozinho.cs
@@ -2,8 +2,17 @@
 using System.Collections;
 
 public class indiozinho : MonoBehaviour { 
-	
+
+	public int velo;
+
 	void Update () {
+
+	}
+
+	void Andando()
+	{
+        
+        transform.position(Vector2.right * velo * Time.deltaTime);
 
 	}
 
